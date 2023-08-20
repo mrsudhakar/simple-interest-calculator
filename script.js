b04ed583c532e41970f6ac77446bf4ef70a4d264
sudhakarmurukuti <murukuti.sudhakar@gmail.com>
2023-08-20 21:31:25 +0530

    These files are replaced with new code

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
+var chart;
+
 function calculate() {
-  const principal = document.getElementById("principal").value;
-  const interestRate = document.getElementById("interest_rate").value;
-  const time = document.getElementById("time").value;
+  var principal = parseFloat(document.getElementById("principal").value);
+  var rate = parseFloat(document.getElementById("rate").value);
+  var time = parseFloat(document.getElementById("time").value);
 
-  const simpleInterest = principal * interestRate * time / 100;
+  var tableBody = document.getElementById("table-body");
+  tableBody.innerHTML = "";
 
-  document.getElementById("result").innerHTML = simpleInterest;
+  var totalAmount = principal;
+  var dataset = [];
+
+  for (var i = 1; i <= time; i++) {
+    var interest = (principal * rate * i) / 100;
+    var row = document.createElement("tr");
+    row.innerHTML = "<td>" + i + "</td><td>" + interest.toFixed(2) + "</td><td>" + (totalAmount + interest).toFixed(2) + "</td>";
+    tableBody.appendChild(row);
+
+    dataset.push(interest);
+    totalAmount += interest;
+  }
+
+  if (chart) {
+    chart.destroy();
+  }
+
+  var chartData = {
+    labels: Array.from({ length: time }, (_, i) => "Year " + (i + 1)),
+    datasets: [{
+      label: "Interest Earned",
+      data: dataset,
+      backgroundColor: [
+        "#FF6384",
+        "#36A2EB",
+        "#FFCE56",
+        "#8CC152",
+        "#4D5360",
+        "#AC92EC",
+        "#FF9F40"
+      ],
+    }],
+  };
+
+  var chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+  };
+
+  var chartElement = document.getElementById("chart");
+  chart = new Chart(chartElement, {
+    type: "pie",
+    data: chartData,
+    options: chartOptions,
+  });
 }
-
-document.getElementById("calculate").addEventListener("click", calculate);
